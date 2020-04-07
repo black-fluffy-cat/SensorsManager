650448c3a6ac3ba8f3a2bbefe4628440d7bf7048
Fluffy Cat <cat.fluffy.black@gmail.com>
2020-04-07 14:13:47 +0200

    Refactor

--- app/src/main/java/com/fluffycat/sensorsmanager/activities/MainActivity.kt
+++ app/src/main/java/com/fluffycat/sensorsmanager/activities/MainActivity.kt
@@ -8,45 +8,51 @@ import androidx.drawerlayout.widget.DrawerLayout
 import com.fluffycat.sensorsmanager.R
 import com.fluffycat.sensorsmanager.navigation_view.MyNavigationItemSelectedListener
 import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
-
-    private var actionBarDrawerToggle: ActionBarDrawerToggle? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setupBarAndNavigationView()
+        setupDrawerViewListener()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        setupNavigationViewListener()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        actionBarDrawerToggle?.apply {
-            if (onOptionsItemSelected(item)) return true
+        // The action bar home/up action should open or close the drawer.
+        when (item.itemId) {
+            android.R.id.home -> {
+                handleToolbarHomeClick()
+                return true
+            }
         }
         return super.onOptionsItemSelected(item)
     }
 
-    private fun setupBarAndNavigationView() {
-        setupActionBarDrawerToggle()
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        setupNavigationView()
+    private fun handleToolbarHomeClick() {
+        mainDrawerLayout.apply {
+            if (isDrawerOpen(navigation)) {
+                closeDrawer(navigation)
+            } else {
+                openDrawer(navigation)
+            }
+        }
     }
 
-    private fun setupActionBarDrawerToggle() {
+    private fun setupDrawerViewListener() {
         val drawerLayout = findViewById<DrawerLayout>(R.id.mainDrawerLayout)
-        actionBarDrawerToggle = ActionBarDrawerToggle(
-                this, drawerLayout, R.string.open, R.string.close).apply {
+        ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close).apply {
             drawerLayout.addDrawerListener(this)
             syncState()
         }
     }
 
-    private fun setupNavigationView() {
+    private fun setupNavigationViewListener() {
         val navigationView = findViewById<NavigationView>(R.id.navigation)
-        navigationView.setNavigationItemSelectedListener(
-                MyNavigationItemSelectedListener(this)
-        )
+        navigationView.setNavigationItemSelectedListener(MyNavigationItemSelectedListener(this))
     }
 }
