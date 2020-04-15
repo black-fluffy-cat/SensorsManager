fb2057300121deb204ffb042562849fca415f905
Fluffy Cat <cat.fluffy.black@gmail.com>
2020-04-15 13:06:58 +0200

    Add GyroscopeListener and GyroscopeSensorController -> Gyroscope chart

--- app/src/main/java/com/fluffycat/sensorsmanager/fragments/GyroscopeFragment.kt
+++ app/src/main/java/com/fluffycat/sensorsmanager/fragments/GyroscopeFragment.kt
@@ -1,17 +1,22 @@
 package com.fluffycat.sensorsmanager.fragments
 
+import android.app.Activity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.fluffycat.sensorsmanager.R
+import com.fluffycat.sensorsmanager.sensors.GyroscopeSensorController
+import com.fluffycat.sensorsmanager.sensors.ISensorController
 
-class GyroscopeFragment: Fragment() {
+class GyroscopeFragment : Fragment() {
 
     companion object {
         const val TAG = "GyroscopeFragmentTAG"
     }
+
+    private lateinit var gyroscopeSensorController: ISensorController
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         setActivityTitle()
@@ -20,5 +25,16 @@ class GyroscopeFragment: Fragment() {
 
     private fun setActivityTitle() {
         activity?.title = getString(R.string.gyroscope)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        gyroscopeSensorController = GyroscopeSensorController(activity as Activity)
+        gyroscopeSensorController.startReceivingData()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        gyroscopeSensorController.stopReceivingData()
     }
 }
