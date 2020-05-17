e29759ad9abdab24aa61232410dca1f454415fc5
Fluffy Cat <cat.fluffy.black@gmail.com>
2020-05-17 21:13:17 +0200

    Add heartbeat chart

--- app/src/main/java/com/fluffycat/sensorsmanager/fragments/HeartbeatFragment.kt
+++ app/src/main/java/com/fluffycat/sensorsmanager/fragments/HeartbeatFragment.kt
@@ -2,9 +2,12 @@ package com.fluffycat.sensorsmanager.fragments
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.graphics.Color
 import android.hardware.SensorEvent
+import android.hardware.SensorManager
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,17 +17,29 @@ import androidx.lifecycle.Observer
 import com.fluffycat.sensorsmanager.R
 import com.fluffycat.sensorsmanager.SensorsManagerApplication
 import com.fluffycat.sensorsmanager.sensors.HeartbeatSensorController
-import com.fluffycat.sensorsmanager.sensors.ISensorController
 import com.fluffycat.sensorsmanager.utils.HEART_RATE_REQUEST_CODE
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
 import kotlinx.android.synthetic.main.heartbeat_fragment.*
+import java.util.*
 
 class HeartbeatFragment : Fragment() {
 
     companion object {
         const val TAG = "HeartbeatFragmentTAG"
+        const val chartTitle = "Heartbeat rate"
     }
 
-    private lateinit var heartbeatSensorController: ISensorController
+    // TODO Change to interface type :(
+    private lateinit var heartbeatSensorController: HeartbeatSensorController
+    private var lineData: LineData
+
+    init {
+        val lineDataSet1: LineDataSet = createDataSet(Color.RED, chartTitle)
+        lineData = LineData(lineDataSet1)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         setActivityTitle()
@@ -45,9 +60,23 @@ class HeartbeatFragment : Fragment() {
 
         heartbeatSensorController = HeartbeatSensorController(context ?: SensorsManagerApplication.getContext())
 
+        // TODO Can I observe it here and forget about it?
         heartbeatSensorController.sensorCurrentData.observe(this, Observer { sensorEvent ->
             onDataChanged(sensorEvent)
         })
+
+        heartbeatSensorController.additionalData.observe(this, Observer { additionalCode ->
+            onAdditionalDataChanged(additionalCode)
+        })
+
+        heartbeatChart.data = lineData
+    }
+
+    @Suppress("SameParameterValue")
+    private fun createDataSet(dataSetColor: Int, label: String) = LineDataSet(ArrayList(), label).apply {
+        setDrawCircles(false)
+        lineWidth = 5f
+        color = dataSetColor
     }
 
     override fun onStart() {
@@ -68,5 +97,25 @@ class HeartbeatFragment : Fragment() {
 
     private fun onDataChanged(sensorEvent: SensorEvent) {
         heartbeatFragmentText.text = sensorEvent.values[0].toString()
+        lineData.apply {
+            addEntry(Entry(getDataSetByIndex(0).entryCount.toFloat(), sensorEvent.values[0]), 0)
+
+            notifyDataChanged()
+            heartbeatChart.notifyDataSetChanged()
+            heartbeatChart.setVisibleXRangeMaximum(100F)
+            heartbeatChart.moveViewTo(entryCount.toFloat(), 0F, YAxis.AxisDependency.RIGHT)
+        }
+    }
+
+    private fun onAdditionalDataChanged(additionalCode: Int) {
+        Log.d(tag, "additionalCode: $additionalCode")
+        when (additionalCode) {
+            SensorManager.SENSOR_STATUS_UNRELIABLE -> {
+                heartbeatFragmentAdditionalText.text = "Please put your finger on the sensor and wait for measurement"
+            }
+            else -> {
+                heartbeatFragmentAdditionalText.text = ""
+            }
+        }
     }
 }
--- app/src/main/java/com/fluffycat/sensorsmanager/listeners/HeartbeatListener.kt
+++ app/src/main/java/com/fluffycat/sensorsmanager/listeners/HeartbeatListener.kt
@@ -4,13 +4,19 @@ import android.hardware.Sensor
 import android.hardware.SensorManager
 import android.os.Build
 import androidx.annotation.RequiresApi
-import com.fluffycat.sensorsmanager.sensors.ISensorController
+import com.fluffycat.sensorsmanager.sensors.HeartbeatSensorController
 
-class HeartbeatListener(dataCollector: ISensorController) : MySensorListener(dataCollector) {
+// TODO Change argument to interface type :(
+class HeartbeatListener(private val heartbeatDataCollector: HeartbeatSensorController) :
+    MySensorListener(heartbeatDataCollector) {
 
     @RequiresApi(Build.VERSION_CODES.KITKAT_WATCH)
     override fun registerListener(sensorManager: SensorManager) {
         sensorManager.registerListener(this,
                 sensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE), SensorManager.SENSOR_DELAY_GAME)
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        heartbeatDataCollector.onAdditionalDataChanged(accuracy)
     }
 }
