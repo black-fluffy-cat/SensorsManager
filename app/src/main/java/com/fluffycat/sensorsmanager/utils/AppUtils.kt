93da2faf4201240dc3990d0940bf5a2ed1ffbf66
Fluffy Cat <cat.fluffy.black@gmail.com>
2020-05-18 11:32:43 +0200

    Add icons to navigation menu

--- app/src/main/java/com/fluffycat/sensorsmanager/utils/AppUtils.kt
+++ app/src/main/java/com/fluffycat/sensorsmanager/utils/AppUtils.kt
@@ -11,4 +11,4 @@ fun showToast(context: Context, message: String) {
 
 val Any.tag: String get() = this.javaClass.simpleName.take(LOG_TAG_MAX_LENGTH)
 
-fun getLicencesInfoString() = ""
+fun getLicencesInfoString() = "Icon made by srip, cursor-creative, flat-icons, freepik, smashicons from www.flaticon.com"
