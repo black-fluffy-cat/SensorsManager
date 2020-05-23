cee9e9a011332c4aa0951167d0412b557fdcdc8d
Fluffy Cat <cat.fluffy.black@gmail.com>
2020-05-23 13:44:45 +0200

    Add heartbeat sensor

---
+++ app/src/main/java/com/fluffycat/sensorsmanager/sensors/HeartbeatSensorController.kt
@@ -0,0 +1,69 @@
+package com.fluffycat.sensorsmanager.sensors
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorManager
+import android.os.Build
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import com.fluffycat.sensorsmanager.listeners.HeartbeatListener
+import com.fluffycat.sensorsmanager.listeners.MySensorListener
+import com.fluffycat.sensorsmanager.utils.tag
+
+class HeartbeatSensorController(context: Context) : ISensorController {
+
+    override val sensorCurrentData = MutableLiveData<SensorEvent>()
+
+    private val sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    private val heartbeatListener: MySensorListener = HeartbeatListener(this)
+
+    override fun startReceivingData() {
+        heartbeatListener.registerListener(sensorManager)
+        Log.d(tag, "Started receiving data")
+    }
+
+    override fun stopReceivingData() {
+        sensorManager.unregisterListener(heartbeatListener)
+        Log.d(tag, "Stopped receiving data")
+    }
+
+    override fun onSensorDataReceived(event: SensorEvent) {
+        sensorCurrentData.value = event
+    }
+
+    override fun getSensorInfo(): String {
+            val sensor = sensorManager.getDefaultSensor(Sensor.TYPE_HEART_BEAT)
+            var infoString = ""
+            sensor?.apply {
+                infoString += "$name\n"
+//                infoString += "fifoReservedEventCount $fifoReservedEventCount\n"
+//                infoString += "fifoMaxEventCount $fifoMaxEventCount\n"
+                infoString += "Maximum range: $maximumRange\n"
+//                infoString += "minDelay: $minDelay\n"
+                infoString += "Power: $power\n"
+                infoString += "Resolution: $resolution\n"
+                infoString += "Type: $type\n"
+                infoString += "Vendor: $vendor\n"
+                infoString += "Version: $version\n"
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT_WATCH) {
+//                    infoString += "stringType: $stringType\n"
+                }
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+//                    infoString += "isWakeUpSensor: $isWakeUpSensor\n"
+//                    infoString += "reportingMode: $reportingMode\n"
+//                    infoString += "maxDelay: $maxDelay\n"
+                }
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//                    infoString += "highestDirectReportRateLevel: $highestDirectReportRateLevel\n"
+                    infoString += "Id: $id\n"
+//                    infoString += "isDynamicSensor: $isDynamicSensor\n"
+//                    infoString += "isAdditionalInfoSupported: $isAdditionalInfoSupported\n"
+                }
+            }
+            return infoString
+    }
+}
